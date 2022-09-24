0abfb2151570812380148901d843fa1369925953
carlosdesenvolvedor <113857909+carlosdesenvolvedor@users.noreply.github.com>
2022-09-24 17:45:28 -0300

    Troquei nome dos botoes de procurar parâmetros arrumei combo box 0,1 etc

--- src/TelaCadastroParmCelofane.java
+++ src/TelaCadastroParmCelofane.java
@@ -2414,7 +2414,7 @@ public class TelaCadastroParmCelofane extends javax.swing.JFrame {
 
         jLabel13.setText("Acondicionamento");
 
-        cboAcondicionamento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { ".", "1", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", " " }));
+        cboAcondicionamento.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", " " }));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
